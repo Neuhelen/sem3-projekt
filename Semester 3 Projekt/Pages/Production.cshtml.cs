a26f0397bd9deada97a4dbf405f6ddab19575a14
Patrick Dahl <patrick.v.d@hotmail.com>
2023-11-03 13:39:12 +0100

    Jeg har added et ASP.Net Core Web App projekt til denne branch. Hvis det skal gøres på en anden måde, så bedes en af jer venligst gøre det. Jeg har ingen ide, hvad jeg ellers skal.

---
+++ Semester 3 Projekt/Pages/Production.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Semester_3_Projekt.Pages
+{
+    public class ProductionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

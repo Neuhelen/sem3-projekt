4fb09583eadda33283410ac58e7751b4f3f755ff
Karl Johan Thomsen <kthom20@student.sdu.dk>
2023-11-07 16:47:36 +0100

    Controller created and communication with simulator established

--- Semester 3 Projekt/Pages/index.cshtml.cs
+++ Semester 3 Projekt/Pages/index.cshtml.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Semester_3_Projekt.controller;
 
 namespace Semester_3_Projekt.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public BeerMachineAPI _beerMachineAPI;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _beerMachineAPI = BeerMachineAPI.Instance;
         }
 
         public void OnGet()
         {
-
+            bool status = _beerMachineAPI.common_post("Cube.Command.MachSpeed", 50f);
         }
     }
 }
---
+++ Semester 3 Projekt/controller/BeerMachineAPI.cs
@@ -0,0 +1,53 @@
+﻿using Opc.UaFx.Client;
+
+namespace Semester_3_Projekt.controller
+{
+    public sealed class BeerMachineAPI
+    {
+        // Singleton shenaniganz
+        private static BeerMachineAPI instance = null;
+        public static BeerMachineAPI Instance { 
+            get { 
+                if (instance == null)
+                {
+                    instance = new BeerMachineAPI();
+                }
+                return instance;
+                }
+        }
+        // Connection variables
+        String connection_string = "opc.tcp://localhost:4840/";
+        OpcClient client = null;
+
+        // API REFS
+        String BASEAPI = "ns=6;s=::Program:";
+
+        // Constructor for initializing the client
+        private BeerMachineAPI()
+        {
+            this.client = new OpcClient(this.connection_string);
+            client.Connect();
+        }
+
+        public Opc.UaFx.OpcValue common_get(String endpoint)
+        {
+            var res = client.ReadNode(this.BASEAPI + endpoint);
+            return res;
+        }
+
+        public bool common_post(String endpoint, object value)
+        {
+            var res = client.WriteNode(this.BASEAPI + endpoint, value);
+            if (res.IsGood)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+
+
+    }
+}

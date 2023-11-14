2e2e8765d0995b3438e319d3f4519347c88d6422
Helge Baad Poulsen <hepou22@student.sdu.dk>
2023-11-14 08:40:23 +0100

    added method to get specific product

---
+++ Semester 3 Projekt/Classes/DBUpdate.cs
@@ -0,0 +1,21 @@
+﻿using Semester_3_Projekt.controller;
+
+namespace Semester_3_Projekt.Classes
+{
+    public class DBUpdate
+    {
+        public BeerDBConn BeerDB;
+        public DBget BeerGet;
+        public DBUpdate()
+        {
+            var BeerDBContextOptions = DBConnHelper.getBeerDBConn();
+            BeerDB = new BeerDBConn(BeerDBContextOptions);
+            BeerGet = new DBget();
+        }
+
+        public void UpdateProduct (int ProductID)
+        {
+            
+        }
+    }
+}

a359225fa6b17122c3773a4c782effd3ff4efafe
Helge Baad Poulsen <hepou22@student.sdu.dk>
2023-12-17 17:44:49 +0100

    added a double value to batch_log

---
+++ Semester 3 Projekt/Migrations/20231217163925_batchlog_addition.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Semester_3_Projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class batchlog_addition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "dValue",
+                table: "BatchLogs",
+                type: "double",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dValue",
+                table: "BatchLogs");
+        }
+    }
+}
